cee099501a755232318819d4b922984f153a7bbe
M-W-Kirk <micah@kirkpatrickcore.com>
2022-04-11 17:53:20 -0400

    updated packages and added the pip freeze package output into requirements.txt

--- first/first.py
+++ first/first.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+'''
+This file is my "Hello World" into the world of the roBERTa model
+'''
+
+# %% 
+# apply byte-pair encoding (BPE) to train a tokenizer
+from pathlib import Path
+from tokenizers import ByteLevelBPETokenizer
+
+tokenizer = ByteLevelBPETokenizer()
+paths = 
+
+# %%
